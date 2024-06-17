da16f46a8bf1c84aec23f290091881052d3d9592
Komal Singhal <84892474+Komal491@users.noreply.github.com>
2024-06-17 15:05:00 +0530

    Create [timestamp]-create-favourite.js

---
+++ migrations/[timestamp]-create-favourite.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Favourites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      state_province: {
+        type: Sequelize.STRING
+      },
+      web_pages: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Favourites');
+  }
+};
